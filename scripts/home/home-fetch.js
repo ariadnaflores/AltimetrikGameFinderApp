f74decf3a91ccce3661c5bdb1bad0f4d8f1475bf
ariadnaflores <ariadnafloresvilchez@gmail.com>
2023-06-12 23:31:27 -0500

    Implements the real time search function

--- scripts/home/home-fetch.js
+++ scripts/home/home-fetch.js
@@ -2,7 +2,7 @@ const apiUrl = 'https://api.rawg.io/api';
 const apiKey = 'c979cee55209403d99dacc61c9eb4883';
 const gameDataElement = document.getElementById('game-data');
 
-// Realizar la solicitud a la API utilizando fetch
+// Realizar la solicitud a la API utilizando fetch para mostrar todos los datos
 fetch(`${apiUrl}/games?key=${apiKey}`)
   .then(response => response.json())
   .then(data => {
@@ -26,3 +26,32 @@ fetch(`${apiUrl}/games?key=${apiKey}`)
     // Manejar errores en caso de que la solicitud falle
     console.log('Error:', error);
   });
+
+// Función para buscar por título de juego
+function searchGamesByTitle(searchTerm) {
+  fetch(`${apiUrl}/games?key=${apiKey}&search=${searchTerm}`)
+    .then(response => response.json())
+    .then(data => {
+      const games = data.results;
+      let html = '';
+
+      games.forEach(game => {
+        html += `<h2>${game.name}</h2>`;
+        html += `<p>Rating: ${game.rating}</p>`;
+        html += `<img src="${game.background_image}" alt="${game.name}" /><br><br>`;
+      });
+
+      gameDataElement.innerHTML = html;
+      console.log(games);
+    })
+    .catch(error => {
+      console.log('Error:', error);
+    });
+}
+
+// Búsqueda en tiempo real
+const searchInput = document.getElementById('search-input');
+searchInput.addEventListener('input', event => {
+  const searchTerm = event.target.value;
+  searchGamesByTitle(searchTerm);
+});
